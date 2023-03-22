49ddc006a1718dd347459136d3f034a7e351aec8
AhmedKamra1 <ahmed.amigo3@gmail.com>
2023-03-23 01:41:30 +0500

    deployment using firebase

--- react-router-6.9.0-jwt-project-14/frontend/src/pages/Logout.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router-dom";
-
-const logoutAction = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expiration');
-    return redirect('/')
-}
-
-export default logoutAction;
