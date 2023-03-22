2ee85b45cc2b017360a9d1823d911d9e1c9f3a91
AhmedKamra1 <ahmed.amigo3@gmail.com>
2023-03-23 00:47:31 +0500

    simple contextapi usage

--- inputrefs-project-3/src/components/UI/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import styles from './Backdrop.module.css'
-
-function Backdrop(props) {
-    return <div className={styles.backdrop} onClick={props.onConfirm}></div>;
-  }
-
-export default Backdrop;
